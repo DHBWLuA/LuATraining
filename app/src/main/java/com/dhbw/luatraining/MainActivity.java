80d1b78005b3e513c308d14073437265c6ba08b1
Maxi <maximilianneubert@msn.com>
2016-08-16 23:05:14 +0200

    Added side drawer

--- app/src/main/java/com/dhbw/luatraining/MainActivity.java
+++ app/src/main/java/com/dhbw/luatraining/MainActivity.java
@@ -1,8 +1,10 @@
 package com.dhbw.luatraining;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +14,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,19 +29,9 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -52,46 +48,39 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (item.getItemId()) {
+            case R.id.nav_mengen:
+                Toast.makeText(getApplicationContext(), "Mengen wurde ausgewählt.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_relationen:
+                Toast.makeText(getApplicationContext(), "Relationen wurde ausgewählt.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_abbildungen:
+                Toast.makeText(getApplicationContext(), "Abbildungen wurde ausgewählt.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_boolesche:
+                Toast.makeText(getApplicationContext(), "Boole´sche Algebra wurde ausgewählt.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_aussagenlogik:
+                Toast.makeText(getApplicationContext(), "Aussagenlogik wurde ausgewählt.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_praedikatenlogik:
+                Toast.makeText(getApplicationContext(), "Prädikatenlogik erster Stufe wurde ausgewählt.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_problems:
+                Toast.makeText(getApplicationContext(), "Eigene Aufgaben wurde ausgewählt.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_stats:
+                Toast.makeText(getApplicationContext(), "Auswertung wurde ausgewählt.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(getApplicationContext(), "Einstellungen wurde ausgewählt.", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
